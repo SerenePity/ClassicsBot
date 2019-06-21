d2c641162293c9521e923dfc3998853d1c5faf7e
Haibo Chang <haibochaang@gmail.com>
2019-06-21 03:11:15 -0400

    Tentative transliteration

--- transliteration/hebrew.py
+++ transliteration/hebrew.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-import traceback
 
-import subprocess
+import transliteration.hebrew_helper.utils
 
 def transliterate(text):
-    try:
-        process = subprocess.Popen(['node', '-e', 'console.log(require(\"node_modules/hebrew-transliteration\").transliterate("' + text + '"))'],
-                           shell=True,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE, universal_newlines=True, encoding='utf8')
-        out, err = process.communicate()
-    except:
-        traceback.print_exc()
-    print("In hebrew.transliterate()")
-    if err:
-        print(err)
-    if out == "" or not out:
-        return "Not found"
-    return out
-
-#transliterate("אֲדֹנָי שְׂפָתַי תִּפְתָּח וּפִי יַגִּיד תְּהִלָּתֶךָ׃")
+    return transliteration.hebrew_helper.utils.transliterate(text)
