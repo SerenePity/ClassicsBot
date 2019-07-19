b6dce161cd9bbdfeecb97650d90c06fefd2ba109
SerenePity <haibochaang@gmail.com>
2019-07-18 22:49:49 -0400

    Gibbon Markov
    Signed-off-by: SerenePity <haibochaang@gmail.com>

--- latin_word_picker/word_picker.py
+++ latin_word_picker/word_picker.py
@@ -3,10 +3,6 @@ from latin_word_picker.meissner_latin import meissner
 import random
 
 complete = list(pie.union(meissner))
-"""
-for i in range(100):
-    print(random.choice(list(complete)))
-"""
 
 def pick_word():
     return random.choice(complete)
