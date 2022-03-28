31061891800f9ae13be9acaa39e2a7cdb7074475
haibo <haibochaang@gmail.com>
2022-03-27 23:38:38 -0400

    Typo

--- robotic_roman.py
+++ robotic_roman.py
@@ -888,7 +888,7 @@ class RoboticRoman():
                 return "Not found"
         if version.strip.lower() == 'cc':
             try:
-                return self.get_cc_verse(verse)
+                return get_cc_passage(verse)
             except:
                 traceback.print_exc()
                 return "Not found"
@@ -911,19 +911,6 @@ class RoboticRoman():
             traceback.print_exc()
             passage = "Not found"
         return passage.strip().replace("Read full chapter", "").replace("\n", " ")
-
-    def get_cc_verse(self, id, book):
-
-        chinese_book = english_to_cc[book.lower().capitalize()]
-
-        url = f"https://zh.wikisource.org/wiki/%E8%81%96%E7%B6%93_(%E6%96%87%E7%90%86%E5%92%8C%E5%90%88)/{chinese_book}"
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, "html.parser")
-
-        def remove_digits(s):
-            return ''.join([i for i in s if not i.isdigit()])
-
-        return remove_digits(soup.find('span', {"id": id}).parent.text).strip()
 
     def get_random_verse_by_testament(self, testament):
         """
